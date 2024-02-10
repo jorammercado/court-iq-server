79fe1e6af1a3587c715e3479fa481ddf81484ab5
arim83 <jorammercado@pursuit.org>
2024-02-10 16:40:45 -0500

    update welcome message

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const BASE_API_URL = process.env.BASE_API_URL;
 app.use(express.json());
 app.use(cors());
 
-
+app.get("/", (req, res) => {
+    res.send("Welcome to capstone")
+  })
 
 const googleMapsController = require("./controllers/googleMapsController")
 app.use("/googleMaps", googleMapsController)
