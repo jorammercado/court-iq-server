345d7a4b6efcaf66e05b921c83ee258fcc0109ec
arim83 <jorammercado@pursuit.org>
2024-02-15 19:58:02 -0500

    update user properties & corresp. files

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,13 @@ CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     displayname VARCHAR(50) DEFAULT 'first last',
-    firstname VARCHAR(50) NOT NULL DEFAULT 'first name unknown',
-    lastname VARCHAR(50) NOT NULL DEFAULt 'last namer unknown',
+    firstname VARCHAR(50) DEFAULT 'first name unknown',
+    lastname VARCHAR(50) DEFAULt 'last name unknown',
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     photourl TEXT DEFAULT 'photourl',
     about TEXT DEFAULT 'about me',
     dob TIMESTAMP DEFAULT '1/1/2024',
+    active BOOLEAN DEFAULT true,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
