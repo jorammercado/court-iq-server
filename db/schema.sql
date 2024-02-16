c08d299439b0ad6de185120af646bd4044b80318
arim83 <jorammercado@pursuit.org>
2024-02-15 19:23:22 -0500

    update users schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,12 +6,13 @@ CREATE DATABASE capstone_dev;
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
+    displayname VARCHAR(50) DEFAULT 'first last',
     firstname VARCHAR(50) NOT NULL DEFAULT 'first name unknown',
     lastname VARCHAR(50) NOT NULL DEFAULt 'last namer unknown',
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     photourl TEXT DEFAULT 'photourl',
     about TEXT DEFAULT 'about me',
-    dob VARCHAR(20) DEFAULT '1/1/2024',
+    dob TIMESTAMP DEFAULT '1/1/2024',
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
